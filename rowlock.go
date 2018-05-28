2109144b7bbf81450a3a2a194b0d687fb2eee3fb
Yuxuan 'fishy' Wang <wang@yuxuan.org>
2018-05-28 14:54:56 -0700

    Define Row type
    And other documentation fixes.

--- rowlock.go
+++ rowlock.go
@@ -7,16 +7,20 @@ import (
 // NewLocker defines a type of function that can be used to create a new Locker.
 type NewLocker func() sync.Locker
 
+// Row is the type of a row.
+//
+// It must be hashable.
+type Row interface{}
+
 // MutexNewLocker is a NewLocker using sync.Mutex.
 func MutexNewLocker() sync.Locker {
 	return new(sync.Mutex)
 }
 
-// RowLock defines a set of row lock.
+// RowLock defines a set of locks.
 //
-// A set of row lock is a set of locks.
-// When you do Lock/Unlock operations, you don't do them on a glogal scale.
-// Instead, a Lock/Unlock operation is operated on a given row/key.
+// When you do Lock/Unlock operations, you don't do them on a global scale.
+// Instead, a Lock/Unlock operation is operated on a given row.
 type RowLock struct {
 	locks      sync.Map
 	lockerPool sync.Pool
@@ -35,15 +39,14 @@ func NewRowLock(f NewLocker) *RowLock {
 
 // Lock locks a row.
 //
-// row must be hashable.
-func (rl *RowLock) Lock(row interface{}) {
+// If this is a new row,
+// a new locker will be created using the NewLocker specified in NewRowLock.
+func (rl *RowLock) Lock(row Row) {
 	rl.getLocker(row).Lock()
 }
 
 // Unlock unlocks a row.
-//
-// row must be hashable.
-func (rl *RowLock) Unlock(row interface{}) {
+func (rl *RowLock) Unlock(row Row) {
 	rl.getLocker(row).Unlock()
 }
 
@@ -51,7 +54,7 @@ func (rl *RowLock) Unlock(row interface{}) {
 //
 // If this is a new row,
 // a new locker will be created using the NewLocker specified in NewRowLock.
-func (rl *RowLock) getLocker(row interface{}) sync.Locker {
+func (rl *RowLock) getLocker(row Row) sync.Locker {
 	newLocker := rl.lockerPool.Get()
 	locker, loaded := rl.locks.LoadOrStore(row, newLocker)
 	if loaded {
